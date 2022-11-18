07c614e9ff73701e5409f78c23c77d45c00355fb
djambulat <jambulatahmadov99@gmail.com>
2022-11-19 02:51:55 +0300

    ceated note model

---
+++ backend/app/notes/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy import Text
+from sqlalchemy import TIMESTAMP
+from sqlalchemy.sql import func
+
+from app.database.conf import Base
+
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(256), nullable=False)
+    body = Column(Text, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now())
