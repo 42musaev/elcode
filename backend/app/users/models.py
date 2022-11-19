ad3f3d32c328635e843e244a1f5a94a24f7def21
Askhab Musaev <42musaev@gmail.com>
2022-11-19 11:53:59 +0300

    update models: User, Note

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import String
 from sqlalchemy import Integer
 from sqlalchemy import TIMESTAMP
 from sqlalchemy import Boolean
+from sqlalchemy.sql import func
 
 from app.database.conf import Base
 
@@ -16,3 +17,5 @@ class User(Base):
     refresh_token = Column(String(length=512), nullable=True)
     expires_token = Column(TIMESTAMP, nullable=True)
     disable = Column(Boolean, server_default=str(False).lower())
+    created_at = Column(TIMESTAMP, server_default=func.now())
+    updated_at = Column(TIMESTAMP, onupdate=func.now())
